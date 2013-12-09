f587a0f757b6f176e9d2710fce18849360636be4
Michael Berg <michael.berg@zalf.de>
2013-12-09 15:16:02 +0100

    one more

--- cluster-test/src/cluster_test/core.clj
+++ cluster-test/src/cluster_test/core.clj
@@ -4,16 +4,22 @@
             [clojure.java.io :as io]))
 
 (defn -main
-  [rows cols & [append? content]]
-  (let [append? (or (edn/read-string append?) false)
-        content (or content "x\n")]
+  [& kvs]
+  (let [{:keys [rows cols append? content newline?]}
+        (reduce (fn [m [k v]]
+                  (assoc m (keyword k) v)) {} (partition 2 kvs))
+
+        append? (or (edn/read-string append?) false)
+        nl? (edn/read-string newline?)
+        newline? (if (nil? nl?) true nl?)
+        content (str (or content "x") (when newline? \newline))]
     (doseq [r (range (edn/read-string rows)) c (range (edn/read-string cols))
             :let [path-to-file (str "data/row-" r "/col-" c ".txt")]]
       (do
         (io/make-parents path-to-file)
-        (spit path-to-file content)))))
+        (spit path-to-file content :append append?)))))
 
 
-#_(-main "10" "10")
+#_(-main :rows "10" :cols "10" :append? "true" :content "a")
 
 
